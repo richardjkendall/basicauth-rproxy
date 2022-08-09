7f1c8a53ec0bf41521a53504de4253c4126c4751
Richard Kendall <richard.kendall@gmail.com>
2022-08-09 19:29:44 +1000

    add more tests

--- test/test.sh
+++ test/test.sh
@@ -10,7 +10,7 @@ echo "TEST 1: user without salt, valid password"
 STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -u cinosalt:$CINOSALT_PW http://basicauth/)
 if test $STATUSCODE -ne 200; then
     echo "TEST 1: failed with status code $STATUSCODE, running again with verbose output"
-    curl -v cinosalt:$CINOSALT_PW http://basicauth/
+    curl -v -u cinosalt:$CINOSALT_PW http://basicauth/
     exit 1
 else
     echo "TEST 1: passed"
@@ -20,8 +20,51 @@ echo "TEST 2: user without salt, invalid password"
 STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -u cinosalt:blah http://basicauth/)
 if test $STATUSCODE -ne 401; then
     echo "TEST 2: failed with status code $STATUSCODE, running again with verbose output"
-    curl -v cinosalt:blah http://basicauth/
+    curl -v -u cinosalt:blah http://basicauth/
     exit 1
 else
     echo "TEST 2: passed"
+fi
+
+# test user with salt
+echo "TEST 3: user with salt, valid password"
+STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -u ci:$CI_PW http://basicauth/)
+if test $STATUSCODE -ne 200; then
+    echo "TEST 3: failed with status code $STATUSCODE, running again with verbose output"
+    curl -v -u ci:$CI_PW http://basicauth/
+    exit 1
+else
+    echo "TEST 3: passed"
+fi
+
+echo "TEST 4: user with salt, invalid password"
+STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -u ci:blah http://basicauth/)
+if test $STATUSCODE -ne 401; then
+    echo "TEST 4: failed with status code $STATUSCODE, running again with verbose output"
+    curl -v -u ci:blah http://basicauth/
+    exit 1
+else
+    echo "TEST 4: passed"
+fi
+
+# test invalid user
+echo "TEST 5: non existent user"
+STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" -u random:random http://basicauth/)
+if test $STATUSCODE -ne 401; then
+    echo "TEST 5: failed with status code $STATUSCODE, running again with verbose output"
+    curl -v -u random:random http://basicauth/
+    exit 1
+else
+    echo "TEST 5: passed"
+fi
+
+# test no user
+echo "TEST 6: no user"
+STATUSCODE=$(curl --silent --output /dev/stderr --write-out "%{http_code}" http://basicauth/)
+if test $STATUSCODE -ne 401; then
+    echo "TEST 6: failed with status code $STATUSCODE, running again with verbose output"
+    curl -v http://basicauth/
+    exit 1
+else
+    echo "TEST 6: passed"
 fi
